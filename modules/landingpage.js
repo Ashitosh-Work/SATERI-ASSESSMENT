d24ed30199a91adefb3628358763465fef2d703e
[Ashitosh Kshirsagar] <[ashitosh.kshirsagar.work@gmail.com]>
2024-12-19 00:11:31 +0530

    update 1

--- modules/landingpage.js
+++ modules/landingpage.js
@@ -2,4 +2,13 @@ function landingpage() {
     console.log("landed");
 }
 
+let formElement = document.getElementById("form");
+
+formElement.addEventListener("submit", (e) => {
+    const email = formElement.email.value;
+    e.preventDefault();
+    console.log(email);
+    formElement.email.value = "";
+})
+
 export { landingpage };
